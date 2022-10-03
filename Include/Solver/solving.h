db02fa335692f0ca2e5363666a64bd5af4001a4d
Vianney Marticou <vianney.marticou@epita.fr>
2022-10-03 16:08:24 +0200

    Format Repot

---
+++ Include/Solver/solving.h
@@ -0,0 +1,8 @@
+#ifndef SUDOKU_SOLVER_H
+#define SUDOKU_SOLVER_H
+
+void print_sudoku(unsigned char sudoku[9][9]);
+int can_be_placed(unsigned char grid[9][9], unsigned char row, unsigned char col, unsigned char num);
+int solve_sudoku(unsigned char grid[9][9], unsigned char row, unsigned char col);
+
+#endif
