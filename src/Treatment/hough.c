42e49b6d8805969411469c2a2f4d81b47f15c7bc
Ugo <ugo.majer@gmail.com>
2022-10-19 13:25:05 +0200

    add branch

---
+++ src/Treatment/hough.c
@@ -0,0 +1,17 @@
+// 3 étapes:
+// - Pour chaque point de contour détécté, détermination de la courbe
+// - Construction de l'accumulateur (les maxima locaux de cette matrice correspondant à des droites probables)
+// - Detection des pics dans l'accumulateur
+
+// Include
+#include <stdlib.h>
+#include "Struct/image.h"
+#include "Struct/pixel.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// Construction de l'accumulateur
+
+Matrix *Constructor(SDL)
+{
+}
