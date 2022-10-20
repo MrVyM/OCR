c2b56d6273e55647b8ba3d70339086c439f092b8
kimo-ui <73913490+kimo-ui@users.noreply.github.com>
2022-10-20 18:11:34 +0200

    Add remarquable points

--- src/Treatment/hough.c
+++ src/Treatment/hough.c
@@ -24,12 +24,15 @@ Matrix *Constructor(Image* image)
 	int width = image->width;
 	int height = image->height;
 
-	// Hauteur maximale de l'accumulateur
+	// Hauteur maximale de l'accumulateur (Varaible)
 	int houghHeight;
 	if (height > width)
 		houghHeight = (int) (sqrt(2) * height) / 2;
 	else
 		houghHeight = (int) (sqrt(2) * width) / 2;
+
+	// Le double de la hauteur (Stable)
+	int doubleHoughHeight = houghTransform * 2;
 
 	// Création de l'accumulateur
 	Matrix * acc = initMatrix(maxTheta,height*2);
@@ -64,8 +67,26 @@ Matrix *Constructor(Image* image)
 			pixel = image->pixels[x][y];
 			if (pixel.r == 0 & pixel.g == 0 & pixel.b == 0)
 			{
+				// Ajout d'un point remarquable
+				
+				// Parcours tous les Theta compris entre 0 et 180
+				for (int t = 0; t < maxTheta; t++)
+				{
+					// Calcul de rho pour chaque theta
+					int r = (int)(((x - centerX) * cosArray[t]) + ((y - centerY) * sinArray[t]));
+
+					// r peut être négatif
+					r += houghHeight;
+
+					if(!(r < 0 || r >= doubleHoughHeight))
+					{
+						acc->value[x][y]++;
+					}
+				}
+				numPoints ++;
+
 			}
-			acc->value[x][y] = 1;
+
 		}
 	}
 	return acc;
@@ -75,6 +96,19 @@ Matrix *Constructor(Image* image)
 void houghTransform(Image* image)
 {
 	Matrix *acc = Constructor(image);
+	
+	// Les coordonées du centre de l'image
+	centerX = width / 2;
+	centerY = height / 2;
+
+	for (int x = 0; x < width; ++x)
+	{
+		for(int y = 0; y < height; ++y)
+		{
+			printf("%d", acc->value[x][y];
+		}
+	}
+	
 	printf("%d",acc->width);
 	return;
 
