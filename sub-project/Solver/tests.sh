1b6cbee8c2f67c534fa14c35f779f66eb7f25630
Vianney Marticou <vianney.marticou@epita.fr>
2022-11-08 11:03:58 +0100

    [SOLVER] Edit the tests.sh

--- sub-project/Solver/tests.sh
+++ sub-project/Solver/tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh   
 
-list="00 01 02 03 04 05 06 no_format no_solvable"
+list=$(ls grids/)
 echo "\nLancement des tests...\n"
 for element in $list    
 do   
-	echo "Lancement du Sudoku Solver : ./solver grid_$element"
-        if ./solver.out grids/grid_$element; then
-                cat grids/grid_$element.result
+	echo "Lancement du Sudoku Solver : ./solver grids/$element"
+        if ./solver.out grids/$element; then
+                cat grids/$element.result
         fi
         echo ""
 done
