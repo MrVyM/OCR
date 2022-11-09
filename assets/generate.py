21077f9ca4b0902b86f351ea9199237767f9fea6
Vianney Marticou <vianney.marticou@epita.fr>
2022-11-09 18:35:00 +0100

    [DATASET] Fix the noise function

--- assets/generate.py
+++ assets/generate.py
@@ -9,14 +9,12 @@ width = 28
 height = 28 
 WHITE = (255, 255, 255)
 
-image = 
-
-# Obtention du contexte graphique
-draw = ImageDraw.Draw(image)
 
 def noise(x,y,theta) :
-
-    draw.point((x,y))
+    if (theta+x+y < 3) :
+        return None
+    for i in range(0,28**2,x+y+theta) :
+        draw.point((i%28,i/14),fill=BLACK)
 
 BLACK = (0, 0, 0)
 #draw.text((20, 20), "Cercle trigonométrique", fill=BLACK)
@@ -30,10 +28,10 @@ for ttf_file in ttf_files:
         for y in range(-2,2) :
             for x in range(-5,5,2) :
                 for number in range(1,10) :
-                    draw = Image.new('RGB', (width, height), WHITE)
+                    image = Image.new('RGB', (width, height), WHITE)
+                    draw = ImageDraw.Draw(image);
                     if (number != 0) : 
                         draw.text((9+x,1+y),str(number),fill=BLACK,font=font)
-
+                    noise(x,y,theta)
                     #image = image.rotate(theta,fillcolor=WHITE)
                     image.save(f"{image_path}/dataset_{name_font}_({x},{y},{theta})_{number}","png")
-                    draw.rectangle([(0,0),(width,height)],fill=WHITE)
