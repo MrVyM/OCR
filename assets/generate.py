0584dd42f64ff1db67982ee6ce727b41553284b1
Vianney Marticou <vianney.marticou@epita.fr>
2022-11-18 23:26:24 +0100

    [DATASET] Generate the final file

--- assets/generate.py
+++ assets/generate.py
@@ -9,6 +9,18 @@ width = 28
 height = 28 
 WHITE = (255, 255, 255)
 
+if os.path.exists(image_path+"/data.txt") : 
+    os.remove(image_path + "/data.txt")
+    print("Data remove...")
+
+def addFile(image,number) :
+    filename = image_path + "/data.txt"
+    f = open(filename, "a")
+    for x in range(width) :
+        for y in range(width) : 
+            f.write(str(0 if image.getpixel((x,y))[0] < 10 else 1))
+    f.write(str(number)+"\n")
+    f.close()
 
 def noise(x,y,theta) :
     if (theta+x+y < 3) :
@@ -23,7 +35,7 @@ ttf_files = glob.glob("Font/*.ttf")
 for ttf_file in ttf_files:
     name_font=(ttf_file.split("/")[-1]).split(".ttf")[0]
     font = ImageFont.truetype(ttf_file, size=22)
-
+    print("Generate for the font :",name_font)
     for theta in range(0,30,10) :
         for y in range(-2,2) :
             for x in range(-5,5,2) :
@@ -32,6 +44,8 @@ for ttf_file in ttf_files:
                     draw = ImageDraw.Draw(image);
                     if (number != 0) : 
                         draw.text((9+x,1+y),str(number),fill=BLACK,font=font)
+                    
                     noise(x,y,theta)
+                    addFile(image,number)
                     #image = image.rotate(theta,fillcolor=WHITE)
                     image.save(f"{image_path}/dataset_{name_font}_({x},{y},{theta})_{number}","png")
