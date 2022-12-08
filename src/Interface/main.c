7d71da53edd990f005b1adafb47fa6837a652e15
AzerX2 <bpt.cor@gmail.com>
2022-12-08 15:50:02 +0100

    mise a jour gui

--- src/Interface/main.c
+++ src/Interface/main.c
@@ -1,45 +1,30 @@
 #include <gtk/gtk.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "Struct/pixel.h"
-#include "Struct/image.h"
-#include "Treatment/grayscale.h"
-#include "Treatment/blur.h"
-#include "Treatment/rotation.h"
-#include "Treatment/resize.h"
-#include "Treatment/thresolding.h"
-#include "Treatment/sobel.h"
-#include "Treatment/morph.h"
-#include "Treatment/contrast.h"
-#include "Treatment/gamma.h"
-#include "Treatment/hough.h"
-#include <err.h>
-#include "Struct/neuralNetwork.h"
-#include "Xor/xor.h"
-#include "Struct/matrix.h"
-#include "Xor/function.h"
+
 
 typedef struct UserInterface
 {
     GtkWindow *window;           
     GtkImage *image;        
-    GtkButton *rotate;     
+    GtkButton *rotate;
+    GtkButton *moov;     
     GtkButton *line;
     GtkButton *traitement;
     GtkButton *resolve;
     GtkProgressBar *progress;
+    GtkEntry *entry;
+    GtkScale *scale;
 } UserInterface;
+
+
+//atof() pour convertir en float
 
 void rotate_image(GtkWidget *widget, gpointer data)
 {
     UserInterface *ui = (UserInterface *)data;
     g_print("Rotate\n");
     // on mets la progress bar à 15
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.15);
-
-    image = rotateImage(image, 10);
-    saveImage(image, "rotation.bmp");
-    gtk_image_set_from_file(GTK_IMAGE(ui->image), "rotation.bmp");
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.2);
+    //on rotate l'image de 10 degré
 }
 
 void line_image(GtkWidget *widget, gpointer data)
@@ -47,8 +32,9 @@ void line_image(GtkWidget *widget, gpointer data)
     UserInterface *ui = (UserInterface *)data;
     g_print("Line\n");
     // on mets la progress bar à 50
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.5);
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.7);
 
+    //je te renvoie le seuil avec donc ui->entry
 }
 
 void traitement_image(GtkWidget *widget, gpointer data)
@@ -56,11 +42,7 @@ void traitement_image(GtkWidget *widget, gpointer data)
     UserInterface *ui = (UserInterface *)data;
     g_print("Traitement\n");
     // on mets la progress bar à 25
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.25);
-
-    grayscaleImage(image);
-    saveImage(image, "grayscale.bmp");
-    gtk_image_set_from_file(GTK_IMAGE(ui->image), "grayscale.bmp");
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.4);
 }
 
 void resolve_image(GtkWidget *widget, gpointer data)
@@ -82,7 +64,49 @@ void change_image(GtkWidget *widget, gpointer data)
     
     gchar *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(widget));
 
+    gchar *filename2 = filename + 1;
+    g_print("%s \n",filename2);
+
     gtk_image_set_from_file(GTK_IMAGE(ui->image), filename);
+
+}
+
+void entry_changed(GtkWidget *widget, gpointer data)
+{
+    UserInterface *ui = (UserInterface *)data;
+    const gchar *sText;
+
+    sText = gtk_entry_get_text(GTK_ENTRY(widget));
+    g_print("Entry changed : %s\n", sText);
+
+    ui->entry = GTK_ENTRY(widget);
+}
+
+void save_image(GtkWidget *widget, gpointer data){
+    UserInterface *ui = (UserInterface *)data;
+    // on save avec saveImage() ui->image
+    g_print("save");
+}
+
+void blur_image(GtkWidget *widget, gpointer data)
+{
+    UserInterface *ui = (UserInterface *)data;
+    g_print("Blur\n");
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.1);
+}
+
+void sobel_image(GtkWidget *widget, gpointer data)
+{
+    UserInterface *ui = (UserInterface *)data;
+    g_print("Sobel\n");
+    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(ui->progress), 0.55);
+}
+
+void modify_scale(GtkWidget *widget, gpointer data){
+    UserInterface *ui = (UserInterface *)data;
+    //gtk_range_get_value(ui->scale);
+    ui->scale = GTK_SCALE(widget);
+    g_print("scale");
 
 }
 
@@ -108,15 +132,25 @@ int main(int argc, char *argv[])
     GtkWindow *window = GTK_WINDOW(gtk_builder_get_object(builder, "window.sudoku"));
     GtkImage *image = GTK_IMAGE(gtk_builder_get_object(builder, "image"));
     GtkButton *rotate = GTK_BUTTON(gtk_builder_get_object(builder, "rotate"));
+    GtkButton *quit = GTK_BUTTON(gtk_builder_get_object(builder, "quit"));
+    GtkButton *blur = GTK_BUTTON(gtk_builder_get_object(builder, "blur"));
+    GtkButton *sobel = GTK_BUTTON(gtk_builder_get_object(builder, "sobel"));
+    GtkButton *save = GTK_BUTTON(gtk_builder_get_object(builder, "save"));
     GtkButton *line = GTK_BUTTON(gtk_builder_get_object(builder, "line"));
     GtkButton *traitement = GTK_BUTTON(gtk_builder_get_object(builder, "traitement"));
     GtkButton *resolve = GTK_BUTTON(gtk_builder_get_object(builder, "resolve"));
+    GtkEntry*entry = GTK_ENTRY(gtk_builder_get_object(builder, "seuil"));
+
+    GtkScale *rotation_scale = GTK_SCALE(gtk_builder_get_object(builder, "moov"));
 
     GtkFileChooserButton *filechooserbutton1 = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "file-chooser"));
 
+    gtk_range_set_range(GTK_RANGE(rotation_scale), -180, 180);
+    gtk_range_set_value(GTK_RANGE(rotation_scale), 0);
+
     // progressbar 
     GtkProgressBar *progressbar = GTK_PROGRESS_BAR(gtk_builder_get_object(builder, "progressbar"));
-
+    //gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progressbar), 0);
     
     // Creates the "Game" structure.
     UserInterface ui =  
@@ -127,16 +161,24 @@ int main(int argc, char *argv[])
         .line = line,
         .traitement = traitement,
         .resolve = resolve,
-        .progress = progressbar
+        .progress = progressbar,
+        .entry = entry,
+        .scale = rotation_scale,
     };
     // Connects event handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(rotate, "clicked", G_CALLBACK(rotate_image), &ui);
+    g_signal_connect(quit, "clicked", G_CALLBACK(gtk_main_quit), &ui);
+    g_signal_connect(blur, "clicked", G_CALLBACK(blur_image), &ui);
+    g_signal_connect(sobel, "clicked", G_CALLBACK(sobel_image), &ui);
+    g_signal_connect(save, "clicked", G_CALLBACK(save_image), &ui);
     g_signal_connect(line, "clicked", G_CALLBACK(line_image), &ui);
     g_signal_connect(traitement, "clicked", G_CALLBACK(traitement_image), &ui);
     g_signal_connect(resolve, "clicked", G_CALLBACK(resolve_image), &ui);
     g_signal_connect(filechooserbutton1, "file-set", G_CALLBACK(change_image), &ui);
+    g_signal_connect(entry, "activate", G_CALLBACK(entry_changed), &ui);
 
+    g_signal_connect(rotation_scale, "value-changed", G_CALLBACK(modify_scale), &ui);
 
     // Runs the main loop.
     gtk_main();
